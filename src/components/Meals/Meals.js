669df6e4c7e82875b919e66a3362abed3629bc3f
codingpartnerNB <nehab5314@gmail.com>
2023-10-25 20:51:22 +0530

    Adding Restaurant Summary

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = ()=>{
+    return(
+        <Fragment>
+            <MealsSummary />
+        </Fragment>
+    );
+}
+
+export default Meals;
